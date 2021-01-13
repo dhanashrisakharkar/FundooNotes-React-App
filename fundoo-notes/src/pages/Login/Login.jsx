b2e101549be2468c13ac14fe6c449b1023e2336c
Dhanashri Sakharkar <dhanashrisakharkar333@gmail.com>
2021-01-13 21:27:09 +0530

    [Dhanahsri] Refactor. Login form page with proper functionality and UI design

--- fundoo-notes/src/pages/Login/Login.jsx
+++ fundoo-notes/src/pages/Login/Login.jsx
@@ -1,18 +1,13 @@
-//import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import React, { Component } from 'react';
 import TextField from '@material-ui/core/TextField';
 import Services from "../../Services/service";
-//import FormControlLabel from '@material-ui/core/FormControlLabel';
-//import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
-//import FormRow from "@material-ui/core/FormRow";
 import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
-//import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import { Card } from '@material-ui/core';
 import "./login.css"
@@ -22,214 +17,218 @@ import Snackbar from "@material-ui/core/Snackbar";
 
 
 class Login extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+      emailError: "",
+      emailFlag: false,
+      passwordError: "",
+      passwordFlag: false,
+      showPassword: false,
+      snackbarOpen: false,
+      snackbarMessage: "",
+    };
+  }
+
+  nextPath(path) {
+    this.props.history.push(path);
+  }
+
+
+
+  clickShowPass = () => {
+    this.setState({
+      ...this.state,
+      showPassword: !this.state.showPassword,
+    });
+  };
+
+  change = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  validate = () => {
+    let isError = false;
+    const errors = {
+      emailError: "",
+      emailFlag: false,
+      passwordError: "",
+      passwordFlag: false,
+    };
+
+    if (this.state.email.length == 0) {
+      errors.emailFlag = true;
+      isError = true;
+      errors.emailError = "Enter your Email ";
+    }
+    if (!/[a-zA-Z0-9._]+[@]{1}[a-zA-Z120-9]*[.]{1}[a-zA-Z]*$/.test(this.state.email)) {
+      errors.emailFlag = true;
+      isError = true;
+      errors.emailError = "Please Enter Correct Email";
+    }
+    if (this.state.password.length == 0) {
+      errors.passwordFlag = true;
+      isError = true;
+      errors.passwordError = "Enter Password";
+    }
+
+    this.setState({
+      ...errors,
+    });
+
+    return isError;
+  };
+  SnackbarClose = (e) => {
+    this.setState({ snackbarOpen: false });
+  };
+
+  handleCloseSnackbar = () => {
+    this.setState({ snackbarOpen: false });
+  };
+
+// submitting data on token and localstorage 
+  onSubmit = (event) => {
+    event.preventDefault();
+    const err = this.validate();
+    if (!err) {
+      this.setState({
         email: "",
-        password: "",
-        emailError: "",
         emailFlag: false,
-        passwordError: "",
+        emailError: "",
+        password: "",
         passwordFlag: false,
-        showPassword: false,
-        snackbarOpen: false,
-          snackbarMessage: "",
+        passwordError: "",
+      });
+      let loginData = {
+        email: this.state.email,
+        password: this.state.password,
       };
-      }
-    
-      nextPath(path) {
-        this.props.history.push(path);
-      }
-    
-      
-    
-      clickShowPass = () => {
-        this.setState({
-          ...this.state,
-          showPassword: !this.state.showPassword,
-        });
-      };
-    
-      change = (e) => {
-        this.setState({
-          [e.target.name]: e.target.value,
-        });
-      };
-    
-      validate = () => {
-        let isError = false;
-        const errors = {
-          emailError: "",
-          emailFlag: false,
-          passwordError: "",
-          passwordFlag: false,
-        };
-    
-        if (this.state.email.length == 0) {
-          errors.emailFlag = true;
-          isError = true;
-          errors.emailError = "Enter your Email ";
-        }
-        if (!/[a-zA-Z0-9._]+[@]{1}[a-zA-Z120-9]*[.]{1}[a-zA-Z]*$/.test(this.state.email)) {
-          errors.emailFlag = true;
-          isError = true;
-          errors.emailError = "Please Enter Correct Email";
-        }
-        if (this.state.password.length == 0) {
-          errors.passwordFlag = true;
-          isError = true;
-          errors.passwordError = "Enter Password";
-        }
-    
-        this.setState({
-          ...errors,
-        });
-    
-        return isError;
-      };
-      SnackbarClose = (e) => {
-        this.setState({ snackbarOpen: false });
-      };
-    
-      handleCloseSnackbar = () => {
-        this.setState({ snackbarOpen: false });
-      };
-    
-      onSubmit = (event) => {
-        event.preventDefault();
-        const err = this.validate();
-        if (!err) {
-          this.setState({
-            email: "",
-            emailFlag: false,
-            emailError: "",
-            password: "",
-            passwordFlag: false,
-            passwordError: "",
-          });
-          let loginData = {
-            email: this.state.email,
-            password: this.state.password,
-          };
-          Services
-            .login(loginData)
-            .then((response) => {
-              if (response.status === 200) {
-                this.setState({
-                  snackbarOpen: true,
-                  snackbarMessage: "Login Succesfully.",
-                });
-                localStorage.setItem("token", response.data.id);
-                setTimeout(() => {
-                  this.props.history.push("/dashboard");
-                }, 2000);
-              } else {
-                this.setState({
-                  snackbarOpen: true,
-                  snackbarMessage: "Enter correct credentials",
-                });
-              }
-            })
-            .catch();
-        }
-      };
+      Services
+        .login(loginData)
+        .then((response) => {
+          if (response.status === 200) {
+            this.setState({
+              snackbarOpen: true,
+              snackbarMessage: "Login Succesfully.",
+            });
+            localStorage.setItem("token", response.data.id);
+            localStorage.setItem("firstName", response.data.firstName);
+            localStorage.setItem("lastName", response.data.lastName);
+            localStorage.setItem("email", response.data.email);
+            setTimeout(() => {
+              this.props.history.push("/dashboard");
+            }, 2000);
+          } else {
+            this.setState({
+              snackbarOpen: true,
+              snackbarMessage: "Enter correct credentials",
+            });
+          }
+        })
+        .catch();
+    }
+  };
 
-
-    render() {
-        return (
-            <div className="wrapper-tool">
-            <Card id='form'>
-                <Container component="main" maxWidth="xs">
-                    <CssBaseline />
-                    <div className="paper">
-                        {/* <Avatar className="Avtar">
+// render the html page for login form
+  render() {
+    return (
+      <div className="wrapper-tool">
+        <Card id='form'>
+          <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <div className="paper">
+              {/* <Avatar className="Avtar">
                             <LockOutlinedIcon />
                         </Avatar> */}
-                        <Typography component="h1" variant="h5" className="head">
-                            Sign In
+              <Typography component="h1" variant="h5" className="head">
+                Sign In
                          </Typography>
-                        <form className="form">
-                            <Grid container spacing={4}>
+              <form className="form">
+                <Grid container spacing={4}>
 
-                                <Grid item xs={12}>
-                                    <TextField
-                                        variant="outlined"
-                                        required
-                                        fullWidth
-                                        id="email"
-                                        label="Email Address"
-                                        name="email"
-                                        autoComplete="email"
-                                        value={this.state.email}
-                                        helperText={this.state.emailError}
-                                        error={this.state.emailFlag}
-                                        onChange={(e) => this.change(e)}
-                                    />
-                                </Grid>
-                                <Grid item xs={12}>
-                                    <TextField
-                                        variant="outlined"
-                                        required
-                                        fullWidth
-                                        id="password"
-                                        label="password"
-                                        name="password"
-                                        autoComplete="current-password"
-                                        value={this.state.password}
-                onChange={(e) => this.change(e)}
-                error={this.state.passwordFlag}
-                helperText={this.state.passwordError}
-                                    />
-                                </Grid>
-                            </Grid >
+                  <Grid item xs={12}>
+                    <TextField
+                      variant="outlined"
+                      required
+                      fullWidth
+                      id="email"
+                      label="Email Address"
+                      name="email"
+                      autoComplete="email"
+                      value={this.state.email}
+                      helperText={this.state.emailError}
+                      error={this.state.emailFlag}
+                      onChange={(e) => this.change(e)}
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                      variant="outlined"
+                      required
+                      fullWidth
+                      id="password"
+                      label="password"
+                      name="password"
+                      autoComplete="current-password"
+                      value={this.state.password}
+                      onChange={(e) => this.change(e)}
+                      error={this.state.passwordFlag}
+                      helperText={this.state.passwordError}
+                    />
+                  </Grid>
+                </Grid >
 
 
-                            <Grid style={{ marginTop: 8, marginBottom: 10 }} >
-                                <Grid style={{ padding: 5 }} container justify="flex-end" >
-                                    <Button
-                                        type="submit"
+                <Grid style={{ marginTop: 8, marginBottom: 10 }} >
+                  <Grid style={{ padding: 5 }} container justify="flex-end" >
+                    <Button
+                      type="submit"
 
-                                        variant="contained"
-                                        color="secondary"
-                                        background-color="deepskyblue"
-                                        onClick={(e) => this.onSubmit(e)}
+                      variant="contained"
+                      color="secondary"
+                      background-color="deepskyblue"
+                      onClick={(e) => this.onSubmit(e)}
 
-                                    >
-                                        Sign In
+                    >
+                      Sign In
                             </Button>
-                                </Grid>
-                            </Grid>
-                            <Grid className="extra-tool"  >
-                                <Grid container justify="flex-start">
+                  </Grid>
+                </Grid>
+                <Grid className="extra-tool"  >
+                  <Grid container justify="flex-start">
 
-                                    <Grid item >
+                    <Grid item >
 
-                                        <Link href="http://localhost:4200/register" color="textPrimary" >
+                      <Link href="http://localhost:4200/register" color="textPrimary" >
 
-                                            create account
+                        create account
                                         </Link>
 
 
-                                    </Grid >
-                                </Grid>
-                            </Grid>
-                            <Grid container justify="flex-end" justify  style={{ marginTop: 8, marginBottom: 10 }} className="link" style={{ padding: -1 }}>
-                                
-                                    <Link href="http://localhost:4200/forgetPassword" color="textPrimary" >
+                    </Grid >
+                  </Grid>
+                </Grid>
+                <Grid container justify="flex-end" justify style={{ marginTop: 8, marginBottom: 10 }} className="link" style={{ padding: -1 }}>
 
-                                        forget password
+                  <Link href="http://localhost:4200/forgetPassword" color="textPrimary" >
+
+                    forget password
                                         </Link>
-                                
-                            </Grid>
-                        </form>
-                    </div>
-                    <Box mt={5}>
 
-                    </Box>
-                </Container>
-            </Card >
+                </Grid>
+              </form>
             </div>
-        );
-    }
+            <Box mt={5}>
+
+            </Box>
+          </Container>
+        </Card >
+      </div>
+    );
+  }
 }
 export default withRouter(Login);
