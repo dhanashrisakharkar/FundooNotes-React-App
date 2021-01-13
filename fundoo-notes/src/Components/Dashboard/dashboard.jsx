c9523d222041798615bb6e16af9d4d4f1e4516e4
Dhanashri Sakharkar <dhanashrisakharkar333@gmail.com>
2021-01-13 23:51:09 +0530

    [Dhanashri] Refactor . Proper component displayed for mount on browser with sending props to another component

--- fundoo-notes/src/Components/Dashboard/dashboard.jsx
+++ fundoo-notes/src/Components/Dashboard/dashboard.jsx
@@ -1,50 +1,44 @@
-import React , { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import AppBar from "../AppBar/Appbar"
 import NoteCreate from "../NoteFunction/CreateNote/createNote"
 import DisplayNote from "../NoteFunction/DisplayNote/DisplayNotes"
 // import SideDrawer from "./Drawer"
 // import DisplayNotes from "./DashbordNotes"
 import Notte from "../NoteFunction/CreateNote/Nots"
-import Service  from '../../Services/NoteService'
+import Service from '../../Services/NoteService'
 import "./DashBoard.css"
 
 
-export default function Home(){
-    const [noteList, setNoteList] = useState([])
+export default function Home() {
+  const [noteList, setNoteList] = useState([])
 
-      
-    const getNote = () => {
-        Service.getAllNotes('userToken')
-          .then((res) => {
-            setNoteList(res.data.data.data);
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-      }
 
-      useEffect(() => {
-        getNote()
-      }, []);
-    
+  const getNote = () => {
+    Service.getAllNotes('userToken')
+      .then((res) => {
+        setNoteList(res.data.data.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
-    return(
-        <div>
-        <AppBar/>
-        {/* <NoteCreate /> */}
-        {/* <SideDrawer/> */}
-        {/* <AddNote/> */}
-        
-        <NoteCreate   GetNote={getNote}   />
-        <div className="card-body text-center" >
+  useEffect(() => {
+    getNote()
+  }, []);
+
+  // returning the pages which has to be on browser
+  return (
+    <div>
+      <AppBar />
+      <NoteCreate GetNote={getNote} />
+      <div className="card-body text-center" >
         <div className="main">
-        <DisplayNote item={noteList} GetNote={getNote} className="coloumn" />
+          <DisplayNote item={noteList} GetNote={getNote} className="coloumn" />
+        </div>
       </div>
-        </div>
-        {/* <Notte/> */}
-        </div>
-    )
+    </div>
+  )
 
 }
 
-  
